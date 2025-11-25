0d807a5a130bd0c07708a4efe87ed17328b65b46
KruegerL <laura_krueger@estudante.sesisenai.org.br>
2025-11-25 09:42:16 -0300

    Feito placa trem

---
+++ trem/sketch_oct7a/env.h
@@ -0,0 +1,17 @@
+#ifndef ENV_H
+#define ENV_H
+
+// --- Configurações WiFi ---
+#define WIFI_SSID "FIESC_IOT_EDU"
+#define WIFI_PASS "8120gv08"
+
+// --- Configurações do broker MQTT ---
+#define BROKER_URL "36e8a0cdfccc4299a8cca62d05fc37c8.s1.eu.hivemq.cloud"
+#define BROKER_PORT 8883
+#define BROKER_USR_ID "LauraKrueger"
+#define BROKER_USR_PASS "LauraKrueger1"
+
+// --- Tópico de velocidade do trem S2 ---
+#define TOPIC_TREMVEL_S2 "TremVel"
+
+#endif
---
+++ trem/sketch_oct7a/sketch_oct7a.ino
@@ -0,0 +1,97 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include "env.h"
+
+// --- Pinos dos LEDs indicadores ---
+#define LED_FRENTE 21     // LED verde (frente)
+#define LED_RE 18         // LED vermelho (ré)
+
+// --- Objetos de comunicação ---
+WiFiClientSecure wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+// --- Função de callback MQTT ---
+void callback(char* topic, byte* payload, unsigned long length);
+
+// --- Conexão ao broker MQTT ---
+void connectMQTT();
+
+void setup() {
+    Serial.begin(115200);
+    delay(500);
+
+    // Inicializa LEDs
+    pinMode(LED_FRENTE, OUTPUT);
+    pinMode(LED_RE, OUTPUT);
+    digitalWrite(LED_FRENTE, LOW);
+    digitalWrite(LED_RE, LOW);
+
+    // Conecta ao WiFi
+    Serial.println("Conectando ao WiFi...");
+    wifiClient.setInsecure();
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println("\nWiFi conectado!");
+
+    // Configura MQTT
+    mqttClient.setServer(BROKER_URL, BROKER_PORT);
+    mqttClient.setCallback(callback);
+    connectMQTT();
+}
+
+void loop() {
+    if (!mqttClient.connected()) {
+        connectMQTT();
+    }
+    mqttClient.loop();
+}
+
+// --- Interpreta comando recebido e indica estado ---
+void callback(char* topic, byte* payload, unsigned long length) {
+    String msg = "";
+    for (int i = 0; i < length; i++) msg += (char)payload[i];
+    msg.trim();
+    int valor = msg.toInt();
+
+    Serial.print("Valor recebido MQTT: ");
+    Serial.println(valor);
+
+    if (valor > 0) {
+        // Trem indo para frente
+        digitalWrite(LED_FRENTE, HIGH);
+        digitalWrite(LED_RE, LOW);
+        Serial.printf("Indicador: FRENTE | Velocidade: %d\n", valor);
+    } else if (valor < 0) {
+        // Trem indo para trás
+        digitalWrite(LED_FRENTE, LOW);
+        digitalWrite(LED_RE, HIGH);
+        Serial.printf("Indicador: RÉ | Velocidade: %d\n", abs(valor));
+    } else {
+        // Trem parado
+        digitalWrite(LED_FRENTE, LOW);
+        digitalWrite(LED_RE, LOW);
+        Serial.println("Indicador: PARADO");
+    }
+}
+
+// --- Conecta e inscreve no tópico MQTT ---
+void connectMQTT() {
+    if (WiFi.status() != WL_CONNECTED) return;
+    Serial.print("Conectando ao broker...");
+    while (!mqttClient.connected()) {
+        if (mqttClient.connect("RailFlow-S2-Trem", BROKER_USR_ID, BROKER_USR_PASS)) {
+            Serial.println("\nConectado ao MQTT!");
+            mqttClient.subscribe(TOPIC_TREMVEL_S2);
+            Serial.print("Inscrito no tópico: ");
+            Serial.println(TOPIC_TREMVEL_S2);
+        } else {
+            Serial.print(".");
+            delay(2000);
+        }
+    }
+}
