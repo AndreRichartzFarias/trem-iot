ddbae5f5d6b9427c549040adf6803492000a613c
Andre Richartz Farias <andre_r_farias@estudante.sesisenai.org.br>
2025-12-04 22:05:42 -0300

    Remove debug topic
    Removed unused debug topic

--- estacao1/sketch_oct7a/env.h
+++ estacao1/sketch_oct7a/env.h
@@ -1,7 +1,6 @@
 #ifndef ENV_H
 #define ENV_H
 
-// ---Environment configuration header file---
 // ---Private WiFi configuration---
 #define WIFI_SSID "FIESC_IOT_EDU"
 #define WIFI_PASS "8120gv08"
@@ -10,14 +9,9 @@
 #define BROKER_URL "36e8a0cdfccc4299a8cca62d05fc37c8.s1.eu.hivemq.cloud" // ---URL Sensitive Data---
 #define BROKER_PORT 8883
 
-// ---Broker user credentials depending on the sensor---
 #define BROKER_USR_ID "AndreFarias" // ---Sensor ID 1---
 #define BROKER_USR_PASS "AndreFarias1" // ---Sensors Password---
 
-// Debug topic used by the sketch
-#define TOPIC_DEBUG "RailFlow/S1/Temp1"
-
-// --- Mappings for in-sketch names (keeps backwards compatibility) ---
 #define MQTT_BROKER_CONN BROKER_URL
 #define MQTT_PORT_CONN BROKER_PORT
 #define MQTT_USER_CONN BROKER_USR_ID
@@ -31,8 +25,5 @@
 #define topicHumiditySensor    "RailFlow/S1/Umid"
 #define topicLuminanceSensor   "RailFlow/S1/Lum"
 #define topicPresenceSensor1   "RailFlow/S1/Presenca1"
-
-// Keep debug topic accessible by the sketch
-#define topicDebug TOPIC_DEBUG
 
 #endif
