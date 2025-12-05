209552b663a289669853b837319d6d559eb7389c
Andre Richartz Farias <andre_r_farias@estudante.sesisenai.org.br>
2025-12-05 08:59:53 -0300

    Clean up MQTT connection definitions in env.h
    Removed backwards compatibility definitions for MQTT broker connection.

--- estacao3/sketch_oct7a/env.h
+++ estacao3/sketch_oct7a/env.h
@@ -7,7 +7,6 @@
 
 #define BROKER_URL "36e8a0cdfccc4299a8cca62d05fc37c8.s1.eu.hivemq.cloud"
 #define BROKER_PORT 8883
-
 #define BROKER_USR_ID "AndreFarias"
 #define BROKER_USR_PASS "AndreFarias1"
 
@@ -22,12 +21,6 @@
 
 // Local presence topic for this station (optional)
 #define topicPresenceSensorLocal "RailFlow/S3/Presenca1"
-
-// Backwards compatibility names used in sketches
-#define MQTT_BROKER_CONN BROKER_URL
-#define MQTT_PORT_CONN BROKER_PORT
-#define MQTT_USER_CONN BROKER_USR_ID
-#define MQTT_PASSWORD_CONN BROKER_USR_PASS
 
 #define WIFI_CONN_SSID WIFI_SSID
 #define WIFI_CONN_PASSWORD WIFI_PASS
